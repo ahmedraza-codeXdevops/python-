33d0463168afbac1053014842827dec3be4f384d
Ahmed Raza <ahmedrazak2511@gmail.com>
2026-09-25 23:18:35 +0530

    Add files via upload

---
+++ object .py
@@ -0,0 +1,16 @@
+class student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+student1 = student()
+
+print(student1.name)
+print(student1.age)
+
+student2 = student()
+print(student2.name)
+print(student2.age)
+
+
+
